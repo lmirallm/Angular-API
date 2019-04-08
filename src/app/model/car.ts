b7d9735a45d0352785f06e24fc52e5843ee9bc46
lmirallm <lmirallm@everis.com>
2019-04-08 13:00:21 +0200

    Create Car entity

---
+++ src/app/model/car.ts
@@ -0,0 +1,12 @@
+export class Car {
+    id : String;
+    brand : String;
+    country : String
+    created_at : Date;
+    last_updated : Date;
+    registration : Date;
+    
+    constructor() {
+    }
+    
+}
